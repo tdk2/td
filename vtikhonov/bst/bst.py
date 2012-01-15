d61ac2981c0b4eef646c29a4cc36df9352341014
Vladimir Tikhonov <lipovan@gmail.com>
2012-01-15 18:20:44 +0200

    added size/height statistics printing

--- vtikhonov/bst/bst.py
+++ vtikhonov/bst/bst.py
@@ -2,6 +2,60 @@ import sys
 sys.path.append(sys.path[0] + '/../hash')
 import utils
 
+# collect element number
+class TreeElementNumberCollector:
+    def __init__(self):
+        self.__number = 0
+
+    def __call__(self, item, stage):
+        if stage == 1:
+            self.__number = self.__number + 1
+
+    def getValue(self):
+        return self.__number
+
+
+# collect element number
+class TreeElementHeightCollector:
+    def __init__(self):
+        self.__current = 0
+        self.__max = 0
+        self.__min = None
+        self.__number = 0
+        self.__pathLenSum = 0
+
+    def __call__(self, item, stage):
+        if stage == 0:
+            self.__current = self.__current + 1
+        elif stage == 1:
+            self.__max = max(self.__max, self.__current)
+
+            # leaf node
+            if (item[BSTree.LeftChildIndex] == None and item[BSTree.RightChildIndex]):
+                self.__pathLenSum += self.__current
+                self.__number = self.__number+1
+                if self.__min == None:
+                    self.__min = self.__max
+                else:
+                    self.__min = min(self.__min, self.__current)
+
+        elif stage == 2:
+            self.__current = self.__current - 1
+        elif stage == 3:
+            pass
+
+    def getHeights(self):
+        return {"max":self.__max, "min":self.__min, "avg":self.__pathLenSum/max(self.__number, 1)};
+
+# generic visitor
+class TreeElementVisitor:
+    def __init__(self, visitors):
+        self.__visitors = visitors
+
+    def __call__(self, item, stage):
+        for visitor in self.__visitors :
+            visitor(item, stage)
+
 #
 class BSTree:
     # initialize the bst by a list of elements
@@ -18,17 +72,17 @@ class BSTree:
             x = self.__root
             while x != None:
                 y = x
-                if self.__cmp(key, x[BSTree.__KeyIndex]):
+                if self.__cmp(key, x[BSTree.KeyIndex]):
                     x = self.__getLeftChild(x)
                 else:
                     x = self.__getRightChild(x)
 
             appendedNode = self.__createNode(key)
-            appendedNode[BSTree.__ParentIndex] = y
-            if self.__cmp(key, y[BSTree.__KeyIndex]):
-                y[BSTree.__LeftChildIndex] = appendedNode
+            appendedNode[BSTree.ParentIndex] = y
+            if self.__cmp(key, y[BSTree.KeyIndex]):
+                y[BSTree.LeftChildIndex] = appendedNode
             else:
-                y[BSTree.__RightChildIndex] = appendedNode
+                y[BSTree.RightChildIndex] = appendedNode
 
         else:
             self.__root = self.__createNode(key)
@@ -62,36 +116,40 @@ class BSTree:
             x = rightY
 
         if x != None:
-            x[BSTree.__ParentIndex] = y[BSTree.__ParentIndex]
+            x[BSTree.ParentIndex] = y[BSTree.ParentIndex]
 
-        if y[BSTree.__ParentIndex] == None:
+        if y[BSTree.ParentIndex] == None:
             #y was the root
             self.__root = x
         else:
             parentY = self.__getParent(y)
-            childIndex = BSTree.__RightChildIndex
+            childIndex = BSTree.RightChildIndex
             if y == self.__getLeftChild(parentY):
-                childIndex = BSTree.__LeftChildIndex
+                childIndex = BSTree.LeftChildIndex
             #todo: verify that y is right child of parentY [consistency check]
             elif y != self.__getRightChild(parentY):
                     raise "Unexpected error"
             parentY[childIndex] = x
 
         if y != node:
-            node[BSTree.__KeyIndex] = y[BSTree.__KeyIndex]
+            node[BSTree.KeyIndex] = y[BSTree.KeyIndex]
 
         return self.__getKey(y)
 
-
     #printing content of the bst in sorted order
-    def inorderWalk(self):
+    def printInorder(self):
+        #command = lambda root: return root[BSTree.KeyIndex]
         print "<",
-        self.__inorderWalk(self.__root)
+        self.__inorderWalk(self.__root, self.__printItem)
         #printing newline
         print ">"
 
     def printStatistics(self):
-        pass
+        sizeCollector = TreeElementNumberCollector()
+        heightCollector = TreeElementHeightCollector()
+        visitor = TreeElementVisitor([sizeCollector, heightCollector])
+        self.__inorderWalk(self.__root, visitor)
+        print "size: %d, heights: " % sizeCollector.getValue(), heightCollector.getHeights()
 
     # private section
 
@@ -108,27 +166,21 @@ class BSTree:
 
     # get key of the node
     def __getKey(self, node):
-        return self.__getLink(node, BSTree.__KeyIndex)
+        return self.__getLink(node, BSTree.KeyIndex)
 
     #
     def __getLeftChild(self, node):
-        return self.__getLink(node, BSTree.__LeftChildIndex)
+        return self.__getLink(node, BSTree.LeftChildIndex)
 
     #
     def __getRightChild(self, node):
-        return self.__getLink(node, BSTree.__RightChildIndex)
+        return self.__getLink(node, BSTree.RightChildIndex)
 
     #
     def __getParent(self, node):
-        return self.__getLink(node, BSTree.__ParentIndex)
+        return self.__getLink(node, BSTree.ParentIndex)
 
-    # recursive algorithm printing sorted keys
-    def __inorderWalk(self, root):
-        if root != None:
-            self.__inorderWalk(root[BSTree.__LeftChildIndex])
-            print root[BSTree.__KeyIndex],
-            self.__inorderWalk(root[BSTree.__RightChildIndex])
-
+ 
     # TREE_SUCCESSOR
     def __findSuccessor(self, node):
         right =  self.__getRightChild(node)
@@ -184,23 +236,36 @@ class BSTree:
             # further search in right subreee
             return self.__search(self.__getRightChild(root), key)
 
-    __KeyIndex = 0
-    __LeftChildIndex = 1
-    __RightChildIndex = 2
-    __ParentIndex = 3
+    # just printing an item
+    def __printItem(self, item, stage):
+        if stage == 1:
+            print item[BSTree.KeyIndex],
+
+
+    # inorder walk
+    def __inorderWalk(self, root, command):
+        if root != None:
+            command(root, 0)
+            self.__inorderWalk(root[BSTree.LeftChildIndex], command)
+            command(root, 1)
+            self.__inorderWalk(root[BSTree.RightChildIndex], command)
+            command(root, 2)
+        else:
+            command(root, 3)
+
+    KeyIndex = 0
+    LeftChildIndex = 1
+    RightChildIndex = 2
+    ParentIndex = 3
 
 if __name__=="__main__":
     #values = [3, 1, 8, 2, 6, 7, 5]
-    values = [utils.Random(0, 10) for x in xrange(100)]
     
-    bst = BSTree([], lambda x, y: x > y)
-    for item in values :
-        bst.insert(item)
-    bst.inorderWalk()
-
-    permutation = utils.random_permutation(values)
-    #print permutation
-    for item in permutation :
-        bst.remove(item)
-        #bst.inorderWalk()
+    MinPow = 6
+    MaxPow = 20
+    values = [utils.Random(0, 2**MaxPow) for x in xrange(2**MaxPow)]
+    for bstSize in xrange(MinPow, MaxPow+1):
+        #values = [utils.Random(0, 1000) for x in xrange(2**bstSize)]
+        bst = BSTree(values[0:2**bstSize], lambda x, y: x < y)
+        bst.printStatistics()
 
