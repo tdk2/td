a4b3f8a3d994108039e105a642ac5b2a697b8631
Vladimir Tikhonov <lipovan@gmail.com>
2012-03-04 16:49:09 +0200

    finished rodcutting calculation

--- vtikhonov/rodcutting/rod_cut.py
+++ vtikhonov/rodcutting/rod_cut.py
@@ -7,8 +7,11 @@ class RodCutting:
    # returns optimal cut and corresponding revenue
    # user code: cut, revenue = findOptimal(...)
    # note: prices is [0..n-1] array of prices of i+1 cuts
-   def findOptimal(self, prices):
-      return ([], self.revenueBottomUp(prices, len(prices)))
+   def findOptimal(self, prices, n = 0):
+      if n == 0:
+         n = len(prices)
+      #return ([], self.revenueBottomUp(prices, len(prices)))
+      return (self.revenueBottomUp(prices, n))
 
    # naive recursive implementation
    # n belongs to [1..len(prices)]
@@ -28,27 +31,29 @@ class RodCutting:
             revenues[n] = max([prices[i] + revenueMemoizeAux(prices, n-i-1, revenues) for i in xrange(n)])
          return revenues[n]
 
-      r = [None if i != 0 else 0 for i in xrange(0, n+1)]     
+      r = [None if i != 0 else 0 for i in xrange(n+1)]     
       return revenueMemoizeAux(prices, n, r)
 
    # using bottom-up recursive algorithm
    # n belongs to [1..len(prices)]
    def revenueBottomUp(self, prices, n):
-      r = [None if i != 0 else 0 for i in xrange(0, n+1)]
+      r = [None if i != 0 else 0 for i in xrange(n+1)]
+      s = [0 for i in xrange(n+1)]
       for i in xrange(1, n+1):
-         r[i] = max([prices[j] + r[i-j-1] for j in xrange(i)])
-      return r[n]
+         rs = [(prices[j] + r[i-j-1], j+1) for j in xrange(i)]
+         r[i], s[i] = max(rs, key = lambda pair: pair[0])
 
+      revenue = r[n]
+      cuts = []
+      while n > 0:
+         cuts.append(s[n])
+         n -= s[n]
+      return (cuts, revenue)
 
-def generatePrices(n):
-   return [2*(i+1) for i in xrange(n)]
 
 if __name__=="__main__":
-    #testSingle()
-    #testMultiple()
-    prices = generatePrices(1000)
     rc = RodCutting()
-    #prices = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
-    cuts, maxRevenue = rc.findOptimal(prices)
+    prices = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+    cuts, maxRevenue = rc.findOptimal(prices, 8)
     print "optimal cuts: ", cuts, ", revenue: ", maxRevenue
 
