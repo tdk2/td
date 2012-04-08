987ff2b71824cfa5e2289000a02f67b46c5b5877
Vladimir Tikhonov <lipovan@gmail.com>
2012-04-08 23:54:57 +0300

    implemented fractional knapsack

--- vtikhonov/knapsack/knapsack.py
+++ vtikhonov/knapsack/knapsack.py
@@ -1,8 +1,8 @@
 """ 
-0-1 knapsack problem solution using dynamic programming.
+0-1 and fractional knapsack problems solution using dynamic programming and greedy approach.
 """
 
-def solveKnapsack(W, items):
+def solve01Knapsack(W, items):
    # m is matrix m[i, w] is maximum value that can be attained with weight <= w
    # using items up to i, see http://en.wikipedia.org/wiki/Knapsack_problem#0-1_knapsack_problem
    m = [[0 for w in xrange(W+1)] for i in xrange(len(items) + 1)]
@@ -30,7 +30,30 @@ def solveKnapsack(W, items):
       itemIndex = itemIndex-1
    return (bestValue, bestItems)
 
+def solveFractionalKnapsack(W, items):
+   # greedy approach means just sort the items by density in descending order
+   #  and choose the items until there is free space in knapsack
+   sortedByDensity = sorted(items, key=lambda item: item[0]/item[1], reverse=True)
+   bestValue = 0
+   remainedVolume = W
+   itemIndex = 0
+   bestItems = []
+   for item in sortedByDensity:
+      if remainedVolume == 0:
+         # the knapsack is empty, nothing to do further
+         break
+      if item[1] > remainedVolume:
+         # the current item doesn't fit into the knapsack empty volume
+         continue
+      bestValue += item[0]
+      remainedVolume -= item[1]
+      bestItems.append(item)
+
+   return (bestValue, bestItems)
 
 if __name__=="__main__":
-   print solveKnapsack(50, [(60, 10), (100, 20), (120, 30)])
+   W = 50
+   items = [(60, 10), (100, 20), (120, 30)]
+   print solve01Knapsack(W, items)
+   print solveFractionalKnapsack(W, items)
 
